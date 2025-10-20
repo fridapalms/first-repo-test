6053b83ba50041f073cf2b046d2e0f65e10609a2
Frida Palm <fridapalm@MacBook-Pro-som-tillhor-Frida.local>
2025-10-20 13:46:40 +0200

    added js array

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+class Animal {
+  species;
+  name;
+  color;
+
+  constructor(species, name, color) {
+    this.species = species;
+    this.name = name;
+    this.color = color;
+  }
+}
+
+const panda = new Animal("panda", "Bob", "black and white");
+const zebra = new Animal ("zebra", "Jodie", "black and white stripes");
+const elephant = new Animal ("elephant", "Clara", "grey");
+const lion = new Animal ("lion", "Chester", "beige");
+const parrot = new Animal ("parrot", "Elisa", "rainbow colored");
+
+const zoo = [panda, zebra, elephant, lion, parrot]
